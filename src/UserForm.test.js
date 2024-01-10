5a6376c9f77d1f1de850132dc2f938df2b1c28e0
Daniel RG <21danielrondong@gmail.com>
2024-01-10 15:06:10 +0100

    App test and one TDD test to UserForm tests

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -47,3 +47,21 @@ test("it calls onUserAdd when the form is submitted", async () => {
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@email.com" });
 });
+
+test("empties the two inputs when form is submitted", () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("jane");
+  user.click(emailInput);
+  user.keyboard("jane@email.com");
+
+  user.click(button);
+
+  expect(nameInput).toHaveValue("");
+  expect(emailInput).toHaveValue("");
+});
